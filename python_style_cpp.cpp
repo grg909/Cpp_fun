365abad8d17313110b13239bab07a4ae60410995
grg909 <grg909@foxmail.com>
2019-10-13 23:59:35 +0900

    101303

---
+++ python_style_cpp.cpp
@@ -0,0 +1,22 @@
+/*
+* Date    : 2019/10/13
+* Author  : WANG JINGE
+* Email   : wang.j.au@m.titech.ac.jp
+* Standard: C++14
+*/
+
+
+using namespace std;
+
+// python版for循环
+for(auto i : myList)
+    cout << i;
+
+// auto动态
+auto x = "Hello, world";
+cout << x;
+
+// 元组
+auto tp = make_tuple(5, 6, 7);
+cout << get<0>(tp);
+
