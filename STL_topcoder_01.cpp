ad5731dbc49f9e2bfe2abea4bf8e79ffe9205615
grg909 <grg909@foxmail.com>
2019-10-14 22:46:39 +0900

    1014

--- STL_topcoder_01.cpp
+++ STL_topcoder_01.cpp
@@ -27,4 +27,43 @@ v.resize(25);
 for(int i = 20; i < 25; i++)
     v[i] = i*2;
 
+// create multidemensional vectors
+vector<vector<int> > matrix(N, vector<int>(M));
 
+// never copying of vector when passing it as a parameter
+void some_function(const vector<int>& v) {} // if change the contents of vector is needed, omit the const
+
+pair< string, pair<int, int> > p;
+string s = p.first;
+int x = p.second.first;
+
+// Objects with these properties(*it, <, ++/--) that are associated with containers are called iterators, just like pointer.
+// STL algorithms use begin() and end(), end iterator is point to first invalid object, not the last object.
+vector<int> v;
+for(auto it = v.begin(); it != v.end(); i++ )
+    *it++;
+
+// reverse
+reverse(v.begin(), v.end())
+
+// find
+vector<int> v;
+for(int i = 0 ; i< 100; i++) v.push_back(i);
+if(find(v.begin(), v.end(), 49) != v.end()) {
+    // get the value
+    value = *find(v.begin(), v.end(), 49);
+    // if want to get the index of found element
+    int i = (find(v.begin(), v.end(), 49)) - v.begin();
+    if(i < v.size()) {}
+}
+
+int data[] = {1,5,2,4,3};
+vector<int> x(data, data+5);
+int v1 = *max_element(x.begin(), x.end());  // value
+int i1 = min_element(x.begin(), x.end()) - x.begin(); // index
+
+sort(x.begin(), x.end());
+sort(x.rbegin(), x.rend());
+
+vector<int> v;
+v.insert(1, 42);  // insert value 42 after the first
