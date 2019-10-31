48831f9b112917c6dcff686921d305e331fd252a
grg909 <grg909@foxmail.com>
2019-11-01 00:01:24 +0900

    103103

---
+++ IO_speedup.cpp
@@ -0,0 +1,15 @@
+/*
+* Date    : 2019/11/1
+* Author  : WANG JINGE
+* Email   : wang.j.au@m.titech.ac.jp
+* Standard: C++14
+*/
+
+
+// for reference http://www.hankcs.com/program/cpp/cin-tie-with-sync_with_stdio-acceleration-input-and-output.html
+static auto x = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
